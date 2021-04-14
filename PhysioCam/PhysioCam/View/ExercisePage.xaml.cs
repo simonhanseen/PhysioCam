ff408582cc8c8fa17be31ce0a3204e0615f51f22
simonhanseen <47739651+simonhanseen@users.noreply.github.com>
2021-04-14 10:31:56 +0200

    Setting up basic layout and camera

---
+++ PhysioCam/PhysioCam/View/ExercisePage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PhysioCam.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ExercisePage : ContentPage
+    {
+        public ExercisePage()
+        {
+            InitializeComponent();
+        }
+
+        private async void ImageButton_Clicked(object sender, EventArgs e)
+        {
+            var photo = await MediaPicker.CapturePhotoAsync();
+        }
+    }
+}
