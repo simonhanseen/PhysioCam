183413915ef03931850685e2ab790b3e6e73c983
soonh1 <johnny_tran_3@hotmail.com>
2021-04-20 18:46:18 +0200

    Program page setup

---
+++ PhysioCam/PhysioCam/View/ProgramPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PhysioCam.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProgramPage : ContentPage
+    {
+        public ProgramPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
