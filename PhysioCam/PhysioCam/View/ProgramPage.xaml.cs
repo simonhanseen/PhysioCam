0ed209c488e0cb0eb651e86e6c2a3c4c4377054a
soonh1 <johnny_tran_3@hotmail.com>
2021-04-20 20:34:15 +0200

    ProgramPage done

--- PhysioCam/PhysioCam/View/ProgramPage.xaml.cs
+++ PhysioCam/PhysioCam/View/ProgramPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,10 +13,30 @@ namespace PhysioCam.View
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ProgramPage : ContentPage
     {
+        public ObservableCollection<string> Items { get; set; }
         public ProgramPage()
         {
             InitializeComponent();
-        }
+            Items = new ObservableCollection<string>
+            {
+                "Item 1",
+                "Item 2",
+                "Item 3",
+                "Item 4",
+                "Item 5"
+            };
 
+            MyListView.ItemsSource = Items;
+        }
+        async void Handle_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            if (e.Item == null)
+                return;
+
+            await DisplayAlert("Item Tapped", "An item was tapped.", "OK");
+
+            //Deselect Item
+            ((ListView)sender).SelectedItem = null;
+        }
     }
 }
