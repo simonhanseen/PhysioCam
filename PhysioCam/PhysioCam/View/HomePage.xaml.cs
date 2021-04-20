574352dcdbc73b1348a9be8ba0d506d07a43d178
dohva <rallethekid@gmail.com>
2021-04-20 21:39:42 +0200

    Add LoginPage, HomePage, SignUpPage

---
+++ PhysioCam/PhysioCam/View/HomePage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PhysioCam.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        private void PatientHistoryBtnHandler(object sender, EventArgs e)
+        {
+            //Nav Link
+        }
+
+        private void NewTrainingProgramBtnHandler(object sender, EventArgs e)
+        {
+            //Nav Link
+        }
+    }
+}
